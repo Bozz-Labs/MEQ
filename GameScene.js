2ff25ed8200583167b7d88ba1d6f5a299b8c5ab5
Bozzyma <QMBoznar@outlook.com>
2022-05-31 17:19:21 -0400

    Update GameScene.js

--- GameScene.js
+++ GameScene.js
@@ -18,8 +18,10 @@ class GameScene extends Phaser.Scene {
 
 		platforms.create(225, 490, 'platform').setScale(1, .3).refreshBody();
 
-		gameState.scoreText = this.add.text(195, 485, 'Megawatts: 0', { fontSize: '15px', fill: '#000000' });
+		gameState.scoreText = this.add.text(20, 485, 'Megawatt Hours: 0', { fontSize: '15px', fill: '#000000' });
     
+        gameState.housesPoweredText = this.add.text(20, 450, '0 Houses powered for a year', { fontSize: '15px', fill: '#000000' });
+
 		gameState.player.setCollideWorldBounds(true);
 
 		this.physics.add.collider(gameState.player, platforms);
@@ -46,17 +48,20 @@ class GameScene extends Phaser.Scene {
 		this.physics.add.collider(bugs, platforms, function (bug) {
 			bug.destroy();
 			gameState.score += 10;
-			gameState.scoreText.setText(`Megawatts: ${gameState.score}`);
+			gamestate.housesPowered += 1;
+			gameState.scoreText.setText(`Megawatt Hours: ${gameState.score}`);
+			gameState.housesPoweredText.setText(`${gameState.housesPowered} houses powered for a year`);
 		})
 
 		this.physics.add.collider(gameState.player, bugs, () => {
 			bugGenLoop.destroy();
 			this.physics.pause();
 			this.add.text(180, 250, 'Game Over', { fontSize: '15px', fill: '#000000' });
-			this.add.text(160, 270, 'Click To Restart', { fontSize: '15px', fill: '#000000' })			
+			this.add.text(160, 270, 'Click To Restart', { fontSize: '15px', fill: '#000000' })	
 
 			this.input.on('pointerup', () => {
 				gameState.score = 0;
+				gamestate.housesPowered = 0;
 				this.scene.restart();
 				console.log('Game Reset')
 			});
